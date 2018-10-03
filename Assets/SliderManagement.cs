fd89e4f4275dfe5b91abc21321ce25a954ca60f2
Sydney Rubin <syd.l.rubin@gmail.com>
2018-10-03 15:38:31 -0700

    Bug fixes in flowchart, added player health bar and mana bar, fixed how all bars appear to be filled at start

--- Assets/SliderManagement.cs
+++ Assets/SliderManagement.cs
@@ -8,9 +8,15 @@ public class SliderManagement : MonoBehaviour {
 
     public Flowchart myFlowchart;
     public Slider chimeraHealthSlider;
+    public Slider playerHealthSlider;
+    public Slider manaSlider;
 
     private float chimeraHealthCurrent;
     private float chimeraHealthPrevious;
+    private float playerHealthCurrent;
+    private float playerHealthPrevious;
+    private float manaCurrent;
+    private float manaPrevious;
 
 
  
@@ -36,4 +42,34 @@ public class SliderManagement : MonoBehaviour {
         chimeraHealthSlider.value = Mathf.MoveTowards(chimeraHealthPrevious,chimeraHealthCurrent,100f);
     }
 
+
+
+    public void UpdatePlayerHealthBar()
+    {
+        //this function must be called AFTER the flowchart has stored the player's "previous" health value 
+        //AND THEN updated player's health based on healing effect OR chimera damage
+
+        //get values from flowchart
+        playerHealthCurrent = myFlowchart.GetFloatVariable("playerHealthCurrent");
+        playerHealthPrevious = myFlowchart.GetFloatVariable("playerHealthPrevious");
+
+        //move the slider
+        playerHealthSlider.value = Mathf.MoveTowards(playerHealthPrevious, playerHealthCurrent, 100f);
+    }
+
+
+
+
+    public void UpdateManaBar()
+    {
+        //this function must be called AFTER the flowchart has stored the "previous" mana amount
+        //AND THEN updated mana value based on spending or regaining mana
+
+        //get values from flowchart
+        manaCurrent = myFlowchart.GetFloatVariable("manaCurrent");
+        manaPrevious = myFlowchart.GetFloatVariable("manaPrevious");
+
+        //move the slider
+        manaSlider.value = Mathf.MoveTowards(manaPrevious, manaCurrent, 100f);
+    }
 }
