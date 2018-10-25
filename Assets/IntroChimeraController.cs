91a65ec5dc4b1d72da19511ea2b3ac2302b6ea06
Sydney Rubin <syd.l.rubin@gmail.com>
2018-10-25 16:08:28 -0700

    added main menu, bug fixes and tweaks in final scene, unfinished intro battle added to build settings

---
+++ Assets/IntroChimeraController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class IntroChimeraController : MonoBehaviour {
+
+    public Flowchart battleFlowchart;
+    public string[] chimeraAttack = new string[] { "cShatter", "cPoison", "cFire"};
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 3))
+        {
+            battleFlowchart.StopAllBlocks();
+            battleFlowchart.ExecuteBlock("One");
+            //just put them all in various blocks for now for the sake of expediency
+
+        }
+        else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 5))
+        {
+            battleFlowchart.StopAllBlocks();
+            battleFlowchart.ExecuteBlock("Two");
+
+        }
+        else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 6))
+        {
+            battleFlowchart.StopAllBlocks();
+            battleFlowchart.ExecuteBlock("Three");
+
+        }
+
+        else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 7))
+        {
+            battleFlowchart.StopAllBlocks();
+            battleFlowchart.ExecuteBlock("Four");
+
+        }
+        else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true))
+        {
+            battleFlowchart.StopAllBlocks();
+            battleFlowchart.ExecuteBlock(chimeraAttack[Random.Range(0, 3)]);
+        }
+
+    }
+}
