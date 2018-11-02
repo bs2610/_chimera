7360d29893b73d218f956f63a142c74dd84d2b0a
Sydney Rubin <syd.l.rubin@gmail.com>
2018-11-02 11:41:30 -0700

    updates to battle flowcharts, added Jose music demos

--- Assets/Scripts/chimeraController.cs
+++ Assets/Scripts/chimeraController.cs
@@ -17,30 +17,36 @@ public class chimeraController : MonoBehaviour {
     void Update()
     {
 
-        if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 3))
+        if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 1))
         {
             battleFlowchart.StopAllBlocks();
             battleFlowchart.ExecuteBlock("One");
             //just put them all in various blocks for now for the sake of expediency
 
         }
-        else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 5))
+        else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 3))
         {
             battleFlowchart.StopAllBlocks();
             battleFlowchart.ExecuteBlock("Two");
 
         }
-        else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 6))
+        else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 4))
         {
             battleFlowchart.StopAllBlocks();
             battleFlowchart.ExecuteBlock("Three");
 
         }
 
-        else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 7))
+        else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetIntegerVariable("playerSpellNumber") == 5))
         {
             battleFlowchart.StopAllBlocks();
             battleFlowchart.ExecuteBlock("Four");
+
+        }
+        else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true) && (battleFlowchart.GetFloatVariable("chimeraHealthCurrent") <50))
+        {
+            battleFlowchart.StopAllBlocks();
+            battleFlowchart.ExecuteBlock("Chimera half health");
 
         }
         else if ((battleFlowchart.GetBooleanVariable("playerIsAttacking") == true))
